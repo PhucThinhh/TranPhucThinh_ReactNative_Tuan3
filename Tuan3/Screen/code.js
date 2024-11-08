0ed0fb7148ef1ba0f0a0407456c2f0aede1e56f7
PhucThinh <tranphucthinhthpt.com>
2024-11-08 08:48:36 +0700

    Tuan3_LabFistScreen

---
+++ Tuan3/Screen/code.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+
+const MyApp = () => {
+  return (
+    <View style={{ flex: 1, backgroundColor: '#00CCF9' }}>
+      <View style={{ flex: 3, justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{
+          width: 130,
+          height: 130,
+          borderRadius: 75,
+          borderWidth: 15,
+          borderColor: 'black', 
+          backgroundColor: 'transparent' 
+        }} />
+      </View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ color: 'black', fontSize: 20, fontWeight: 'bold' }}>GROW</Text>
+        <Text style={{ color: 'black', fontSize: 20, fontWeight: 'bold' }}>YOUR BUSINESS</Text>
+      </View>
+      <View style={{ flex: 2, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ color: 'black', fontSize: 12, fontWeight: 'bold' }}>We will help you to grow your business using</Text>
+        <Text style={{ color: 'black', fontSize: 12, fontWeight: 'bold' }}>online server</Text>
+      </View>
+      <View style={{ flex: 2, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center' }}>
+        <TouchableOpacity style={{ backgroundColor: '#e3c000', paddingVertical: 10, paddingHorizontal: 25, borderRadius: 10 }}>
+          <Text style={{ color: 'black', fontWeight: 'bold' }}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ backgroundColor: '#e3c000', paddingVertical: 10, paddingHorizontal: 20, borderRadius: 10 }}>
+          <Text style={{ color: 'black', fontWeight: 'bold' }}>SIGN UP</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default MyApp;
